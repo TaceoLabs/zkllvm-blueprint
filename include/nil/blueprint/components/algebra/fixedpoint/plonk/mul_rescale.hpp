9e9ca0fc68adefc5424358c079b7b4d03cf1d6db
Roman Walch <walch@taceo.io>
2023-10-05 16:26:51 +0200

    .

--- include/nil/blueprint/components/algebra/fixedpoint/plonk/mul_rescale.hpp
+++ include/nil/blueprint/components/algebra/fixedpoint/plonk/mul_rescale.hpp
@@ -3,6 +3,12 @@
 
 #include <nil/crypto3/zk/snark/arithmetization/plonk/constraint_system.hpp>
 
+#include <nil/blueprint/blueprint/plonk/assignment.hpp>
+#include <nil/blueprint/blueprint/plonk/circuit.hpp>
+#include <nil/blueprint/component.hpp>
+#include <nil/blueprint/manifest.hpp>
+#include <nil/blueprint/basic_non_native_policy.hpp>
+
 namespace nil {
     namespace blueprint {
         namespace components {
@@ -23,8 +29,163 @@ namespace nil {
                 using var = typename component_type::var;
                 using manifest_type = plonk_component_manifest;
 
-                // TACEO_TODO: Implement the class here
+                class gate_manifest_type : public component_gate_manifest {
+                public:
+                    std::uint32_t gates_amount() const override {
+                        return mul_rescale::gates_amount;
+                    }
+                };
+
+                static gate_manifest get_gate_manifest(std::size_t witness_amount, std::size_t lookup_column_amount) {
+                    static gate_manifest manifest = gate_manifest(gate_manifest_type());
+                    return manifest;
+                }
+
+                // TACEO_TODO replace 3 with actual witness numbers
+                static manifest_type get_manifest() {
+                    static manifest_type manifest =
+                        manifest_type(std::shared_ptr<manifest_param>(new manifest_single_value_param(3)), false);
+                    return manifest;
+                }
+
+                constexpr static std::size_t get_rows_amount(std::size_t witness_amount,
+                                                             std::size_t lookup_column_amount) {
+                    return 1;
+                }
+
+                // TACEO_TODO replace
+                constexpr static const std::size_t gates_amount = 1;
+                const std::size_t rows_amount = get_rows_amount(this->witness_amount(), 0);
+                struct input_type {
+                    var x = var(0, 0, false);
+                    var y = var(0, 0, false);
+                };
+
+                // TACEO_TODO replace
+                struct result_type {
+                    var output = var(0, 0, false);
+                    result_type(const mul_rescale &component, std::uint32_t start_row_index) {
+                        output = var(component.W(2), start_row_index, false, var::column_type::witness);
+                    }
+
+                    result_type(const mul_rescale &component, std::size_t start_row_index) {
+                        output = var(component.W(2), start_row_index, false, var::column_type::witness);
+                    }
+                };
+
+                template<typename ContainerType>
+                mul_rescale(ContainerType witness) : component_type(witness, {}, {}, get_manifest()) {};
+
+                template<typename WitnessContainerType, typename ConstantContainerType,
+                         typename PublicInputContainerType>
+                mul_rescale(WitnessContainerType witness, ConstantContainerType constant,
+                            PublicInputContainerType public_input) :
+                    component_type(witness, constant, public_input, get_manifest()) {};
+
+                mul_rescale(std::initializer_list<typename component_type::witness_container_type::value_type>
+                                witnesses,
+                            std::initializer_list<typename component_type::constant_container_type::value_type>
+                                constants,
+                            std::initializer_list<typename component_type::public_input_container_type::value_type>
+                                public_inputs) :
+                    component_type(witnesses, constants, public_inputs, get_manifest()) {};
             };
+
+            template<typename BlueprintFieldType, typename ArithmetizationParams>
+            using plonk_fixedpoint_mul_rescale =
+                mul_rescale<crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType, ArithmetizationParams>,
+                            BlueprintFieldType, basic_non_native_policy<BlueprintFieldType>>;
+
+            template<typename BlueprintFieldType, typename ArithmetizationParams>
+            typename plonk_fixedpoint_mul_rescale<BlueprintFieldType, ArithmetizationParams>::result_type
+                generate_assignments(
+                    const plonk_fixedpoint_mul_rescale<BlueprintFieldType, ArithmetizationParams> &component,
+                    assignment<crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType, ArithmetizationParams>>
+                        &assignment,
+                    const typename plonk_fixedpoint_mul_rescale<BlueprintFieldType, ArithmetizationParams>::input_type
+                        instance_input,
+                    const std::uint32_t start_row_index) {
+
+                const std::size_t j = start_row_index;
+
+                // TACEO_TODO extend
+                assignment.witness(component.W(0), j) = var_value(assignment, instance_input.x);
+                assignment.witness(component.W(1), j) = var_value(assignment, instance_input.y);
+                assignment.witness(component.W(2), j) =
+                    var_value(assignment, instance_input.x) * var_value(assignment, instance_input.y);
+                return typename plonk_fixedpoint_mul_rescale<BlueprintFieldType, ArithmetizationParams>::result_type(
+                    component, start_row_index);
+            }
+
+            template<typename BlueprintFieldType, typename ArithmetizationParams>
+            void generate_gates(
+                const plonk_fixedpoint_mul_rescale<BlueprintFieldType, ArithmetizationParams> &component,
+                circuit<crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType, ArithmetizationParams>> &bp,
+                assignment<crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType, ArithmetizationParams>>
+                    &assignment,
+                const typename plonk_fixedpoint_mul_rescale<BlueprintFieldType, ArithmetizationParams>::input_type
+                    &instance_input,
+                const std::size_t first_selector_index) {
+
+                using var = typename plonk_fixedpoint_mul_rescale<BlueprintFieldType, ArithmetizationParams>::var;
+
+                // TACEO_TODO extend
+                auto constraint_1 =
+                    bp.add_constraint(var(component.W(0), 0) * var(component.W(1), 0) - var(component.W(2), 0));
+
+                bp.add_gate(first_selector_index, {constraint_1});
+            }
+
+            template<typename BlueprintFieldType, typename ArithmetizationParams>
+            void generate_copy_constraints(
+                const plonk_fixedpoint_mul_rescale<BlueprintFieldType, ArithmetizationParams> &component,
+                circuit<crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType, ArithmetizationParams>> &bp,
+                assignment<crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType, ArithmetizationParams>>
+                    &assignment,
+                const typename plonk_fixedpoint_mul_rescale<BlueprintFieldType, ArithmetizationParams>::input_type
+                    &instance_input,
+                const std::size_t start_row_index) {
+
+                using var = typename plonk_fixedpoint_mul_rescale<BlueprintFieldType, ArithmetizationParams>::var;
+
+                // TACEO_TODO extend
+                const std::size_t j = start_row_index;
+                var component_x = var(component.W(0), static_cast<int>(j), false);
+                var component_y = var(component.W(1), static_cast<int>(j), false);
+                bp.add_copy_constraint({instance_input.x, component_x});
+                bp.add_copy_constraint({component_y, instance_input.y});
+            }
+
+            template<typename BlueprintFieldType, typename ArithmetizationParams>
+            typename plonk_fixedpoint_mul_rescale<BlueprintFieldType, ArithmetizationParams>::result_type
+                generate_circuit(
+                    const plonk_fixedpoint_mul_rescale<BlueprintFieldType, ArithmetizationParams> &component,
+                    circuit<crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType, ArithmetizationParams>> &bp,
+                    assignment<crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType, ArithmetizationParams>>
+                        &assignment,
+                    const typename plonk_fixedpoint_mul_rescale<BlueprintFieldType, ArithmetizationParams>::input_type
+                        &instance_input,
+                    const std::size_t start_row_index) {
+
+                // TACEO_TODO extend
+                auto selector_iterator = assignment.find_selector(component);
+                std::size_t first_selector_index;
+
+                if (selector_iterator == assignment.selectors_end()) {
+                    first_selector_index = assignment.allocate_selector(component, component.gates_amount);
+                    generate_gates(component, bp, assignment, instance_input, first_selector_index);
+                } else {
+                    first_selector_index = selector_iterator->second;
+                }
+
+                assignment.enable_selector(first_selector_index, start_row_index);
+
+                generate_copy_constraints(component, bp, assignment, instance_input, start_row_index);
+
+                return typename plonk_fixedpoint_mul_rescale<BlueprintFieldType, ArithmetizationParams>::result_type(
+                    component, start_row_index);
+            }
+
         }    // namespace components
     }        // namespace blueprint
 }    // namespace nil
