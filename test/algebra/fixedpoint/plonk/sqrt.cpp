4eb26a9c17144994a38d5fe54add40f3f03ca283
Roman Walch <walch@taceo.io>
2023-11-06 16:47:03 +0100

    sqrt_floor gadget

--- test/algebra/fixedpoint/plonk/sqrt.cpp
+++ test/algebra/fixedpoint/plonk/sqrt.cpp
@@ -17,6 +17,7 @@
 #include <nil/blueprint/blueprint/plonk/circuit.hpp>
 #include <nil/blueprint/components/algebra/fixedpoint/type.hpp>
 #include <nil/blueprint/components/algebra/fixedpoint/plonk/sqrt.hpp>
+#include <nil/blueprint/components/algebra/fixedpoint/plonk/sqrt_floor.hpp>
 
 #include "../../../test_plonk_component.hpp"
 
@@ -90,6 +91,66 @@ void test_fixedpoint_sqrt(FixedType input) {
         component_instance, public_input, result_check, instance_input);
 }
 
+template<typename FixedType>
+void test_fixedpoint_sqrt_floor(FixedType input) {
+    using BlueprintFieldType = typename FixedType::field_type;
+    constexpr std::size_t WitnessColumns = 10;
+    constexpr std::size_t PublicInputColumns = 1;
+    constexpr std::size_t ConstantColumns = 0;
+    constexpr std::size_t SelectorColumns = 1;
+    using ArithmetizationParams = crypto3::zk::snark::
+        plonk_arithmetization_params<WitnessColumns, PublicInputColumns, ConstantColumns, SelectorColumns>;
+    using ArithmetizationType = crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType, ArithmetizationParams>;
+    using hash_type = nil::crypto3::hashes::keccak_1600<256>;
+    constexpr std::size_t Lambda = 40;
+    using AssignmentType = nil::blueprint::assignment<ArithmetizationType>;
+
+    using var = crypto3::zk::snark::plonk_variable<typename BlueprintFieldType::value_type>;
+
+    using component_type =
+        blueprint::components::fix_sqrt_floor<ArithmetizationType,
+                                              BlueprintFieldType,
+                                              nil::blueprint::basic_non_native_policy<BlueprintFieldType>>;
+
+    typename component_type::input_type instance_input = {var(0, 0, false, var::column_type::public_input)};
+
+    double expected_res_f = sqrt(input.to_double());
+    auto expected_res = input.sqrt(true);
+
+    auto result_check = [&expected_res, &expected_res_f, input](AssignmentType &assignment,
+                                                                typename component_type::result_type &real_res) {
+        auto real_res_ = FixedType(var_value(assignment, real_res.output), FixedType::SCALE);
+        double real_res_f = real_res_.to_double();
+#ifdef BLUEPRINT_PLONK_PROFILING_ENABLED
+        std::cout << "fixed_point sqrt_floor test: "
+                  << "\n";
+        std::cout << "input_f : " << input.to_double() << "\n";
+        std::cout << "input   : " << input.get_value().data << "\n";
+        std::cout << "expected: " << expected_res_f << "\n";
+        std::cout << "real    : " << real_res_f << "\n\n";
+#endif
+        if (!doubleEquals(expected_res_f, real_res_f, EPSILON) || expected_res != real_res_) {
+            std::cout << "expected        : " << expected_res.get_value().data << "\n";
+            std::cout << "real            : " << real_res_.get_value().data << "\n\n";
+            std::cout << "expected (float): " << expected_res_f << "\n";
+            std::cout << "real (float)    : " << real_res_f << "\n\n";
+            abort();
+        }
+    };
+    std::vector<std::uint32_t> witness_list;
+    witness_list.reserve(WitnessColumns);
+    for (auto i = 0; i < WitnessColumns; i++) {
+        witness_list.push_back(i);
+    }
+    // Is done by the manifest in a real circuit
+    component_type component_instance(
+        witness_list, std::array<std::uint32_t, 0>(), std::array<std::uint32_t, 0>(), FixedType::M_1, FixedType::M_2);
+
+    std::vector<typename BlueprintFieldType::value_type> public_input = {input.get_value()};
+    nil::crypto3::test_component<component_type, BlueprintFieldType, ArithmetizationParams, hash_type, Lambda>(
+        component_instance, public_input, result_check, instance_input);
+}
+
 template<typename FieldType, typename RngType>
 FieldType generate_random_for_fixedpoint(uint8_t m1, uint8_t m2, RngType &rng) {
     using distribution = boost::random::uniform_int_distribution<uint64_t>;
@@ -116,6 +177,7 @@ void test_components_on_random_data(RngType &rng) {
                 FixedType::SCALE);
 
     test_fixedpoint_sqrt<FixedType>(x);
+    test_fixedpoint_sqrt_floor<FixedType>(x);
 }
 
 template<typename FixedType>
@@ -123,6 +185,7 @@ void test_components(int i) {
     FixedType x((int64_t)i);
 
     test_fixedpoint_sqrt<FixedType>(x);
+    test_fixedpoint_sqrt_floor<FixedType>(x);
 }
 
 template<typename FixedType, std::size_t RandomTestsAmount>
