9e85f7dc14ddf4507fc87c3396fcc3c65f215214
fnieddu <118167989+fnieddu@users.noreply.github.com>
2024-01-09 12:14:51 +0100

    feat: add tan fixedpoint gadget (#8)
    * feat: Add tan fixedpoint gadget
    * chore: more reference_wrappers for fixedpoint components
    * fix: make test_plonk_component.hpp compile again
    * chore: BUILD_BLUEPRINT_TESTS CMakeLists.txt switch
    
    ---------
    
    Co-authored-by: romanmarkusholler <100381970+romanmarkusholler@users.noreply.github.com>

--- test/test_plonk_component.hpp
+++ test/test_plonk_component.hpp
@@ -103,20 +103,16 @@ namespace nil {
 
         template<typename fri_type, typename FieldType>
         typename fri_type::params_type create_fri_params(std::size_t degree_log, const std::size_t expand_factor = 4, const std::size_t max_step = 1) {
-            typename fri_type::params_type params;
             math::polynomial<typename FieldType::value_type> q = {0, 0, 1};
 
             std::size_t r = degree_log - 1;
 
             std::vector<std::shared_ptr<math::evaluation_domain<FieldType>>> domain_set =
                 math::calculate_domain_set<FieldType>(degree_log + expand_factor, r);
+            std::size_t max_degree = (1 << degree_log) - 1;
+            auto step_list = generate_random_step_list(r, max_step);
 
-            params.r = r;
-            params.D = domain_set;
-            params.max_degree = (1 << degree_log) - 1;
-            params.step_list = generate_random_step_list(r, max_step);
-
-            return params;
+            return typename fri_type::params_type(max_degree, domain_set, step_list, expand_factor);
         }
 
         template<typename ComponentType, typename BlueprintFieldType, typename ArithmetizationParams>
